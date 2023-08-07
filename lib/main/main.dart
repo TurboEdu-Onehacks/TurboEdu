ec7f903b074d88ae290b7c57a193fb6891bc6918
0Armaan025 <armaan33000@gmail.com>
2023-08-07 19:05:12 +0530

    update: fixed bugs

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,3 +1,4 @@
+import 'package:edunation/features/auth/screens/signup/signup_screen.dart';
 import 'package:edunation/features/meditation/screens/mediation_home_screen.dart';
 import 'package:edunation/features/meditation_activities_screen.dart/screens/meditation_activites_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MeditationHomeScreen(),
+      home: SignUpScreen(),
     );
   }
 }
